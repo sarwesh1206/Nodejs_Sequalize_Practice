dcc621934bd0c0c9aee58a27ce2bddf3e829f0f9
Sachin <sachin.2019@tecorb.co>
2020-04-29 13:00:43 +0530

    initialize project

---
+++ models/employee.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Employee = sequelize.define('Employee', {
+    name: DataTypes.STRING
+  }, {});
+  Employee.associate = function(models) {
+    // associations can be defined here
+  };
+  return Employee;
+};
